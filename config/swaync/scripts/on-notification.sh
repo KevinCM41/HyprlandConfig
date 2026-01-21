c501def6e372886a688cb034047c91893678126c
KevinCM41 <carballomarrugo03@gmail.com>
2026-01-20 23:25:32 -0500

    Custom SFX for SwayNC + Waybar integration

---
+++ config/swaync/scripts/on-notification.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Play notification sound
+pw-play ~/.config/swaync/gta_sa_notification_sound.mp3
+
+# Optional: Log notification
+# echo "$(date): Notification received" >> ~/.local/share/swaync/notifications.log
